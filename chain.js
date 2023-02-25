dc2a78e7dc29940cd97007a973e8051f72837296
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-02-25 18:39:01 +0600

    chain

---
+++ chain.js
@@ -0,0 +1,26 @@
+const users = [{ id: 1, name: 'Adnan', dg: 'programmer', hobbit: 'codding', birthday: '28 aug 1998' }]
+// console.log(users[0]);
+// console.log(users[0].birthday);
+
+
+const data  = {
+   count: 5000,
+   data:[
+      {id:1, name: 'adnan', job: 'programmer'},
+      {id:2, name: 'hassan', job: 'programmer'},
+   ]
+};
+const dataInfo = (data.data[1].name);
+
+const user = {
+   id: 12,
+   name: 'Adnan Hassan',
+   address: {
+      street: '36/3 Uttara Dhaka',
+      postoffice: 'Uttara',
+   },
+   city:'Dhaka'
+};
+
+console.log(user.address.postoffice);
+
